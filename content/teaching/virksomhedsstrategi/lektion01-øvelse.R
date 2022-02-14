79fba5743b8dd5c46e1069235c15de8a722aef9a
GamerdingerA <alex.gamerdinger@t-online.de>
2022-02-14 12:48:47 +0100

    add course material

---
+++ content/teaching/virksomhedsstrategi/lektion01-øvelse.R
@@ -0,0 +1,28 @@
+################
+#
+# Uge 7: lektion01-øvelse
+#
+################
+
+# Pakker og working directory ---------------------------------------------
+# Load alle pakker vi har brugt for her og setwd() til din working directory
+
+
+# Load data ---------------------------------------------------------------
+
+
+### SPØRGSMÅL 2 ###
+# Hvilke affiliationer har de fleste? -------------------------------------
+
+
+# Hvor mange kvinder er i data sæt (i %) ----------------------------------
+
+
+# Hvilken person har de fleste affiliationer i hele data sæt? -------------
+
+### SPØRGSMÅL 3 ###
+# Data subset "Political" -------------------------------------------------
+
+
+
+
