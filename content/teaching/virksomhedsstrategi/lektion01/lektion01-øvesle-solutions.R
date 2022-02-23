7896bec2d7acb19d22a4abf9b4d587ac7d428a1d
GamerdingerA <alex.gamerdinger@t-online.de>
2022-02-23 14:06:15 +0100

    rename file

--- content/teaching/virksomhedsstrategi/lektion01/lektion01-øvesle-solutions.R
+++ content/teaching/virksomhedsstrategi/lektion01/lektion01-øvesle-solutions.R
@@ -13,74 +13,70 @@ setwd("/Users/alexandergamerdinger/Desktop/PhD/website/agamerdinger/content/teac
 
 # Load data ---------------------------------------------------------------
 
-den <- fread("den17-no-nordic-letters.csv")
+den <- read_csv("den17-no-nordic-letters.csv")
 
 ### SPØRGSMÅL 2 ###
 # Hvilke styrelser (affiliation) har de fleste medlemmer? -------------------------------------
-
-# data.table
-affil <- den[,.N,.(affiliation)]
-affil[order(-N)]
 
 #tidyverse 
 den %>% 
   group_by(affiliation) %>% 
   summarize(N = n()) %>% 
-  arrange(desc(N)) %>% 
-  slice_max(n=1, order_by = N)
+  arrange(desc(N)) 
+# Svaret er H.M. Dronningens 75-aars foedselsdag
 
 # Hvor mange kvinder findes der i datasæt? (i %) ----------------------------------
 
-# data.table
-gender <- den[,.N,.(gender)]
-gender$Percent <- round(gender$N/sum(gender$N), 2)*100
-gender
+# Option 1
+den %>% 
+  count(gender, sort=TRUE) %>% 
+  mutate(Percent = round(n/sum(n), 2)*100) #the round() function rounds the number to 2 digits. 
 
-#tidyverse 
+# Option 2
 den %>% 
   group_by(gender) %>% 
-  summarize(N = n()) %>% 
-  mutate(Percent = round(N/sum(N), 2)*100)
+  summarize(n = n()) %>% 
+  mutate(Percent = round(n/sum(n), 2)*100)
+
+# Svaret er 28 %
 
 # Hvem sidder i de fleste styrelser? -------------
 
-# data.table
-name <- den[,.N,.(name)]
-name[order(-N)]
+# Option 1
+den %>% 
+  count(name, sort = TRUE)
 
-#tidyverse 
+# Option 2
 den %>% 
   group_by(name) %>% 
   summarize(N = n()) %>% 
   arrange(desc(N))
 
+# Svaret er Claus Jensen 6395
+
 ### SPØRGSMÅL 3 ###
 # Lav et nyt datasæt “den1” hvor I kigger kun på aktørene i sektor “Parliament” -------------------------------------------------
 
-# data.table
-den1 <- den[sector == "Parliament"]
-
-#tidyverse 
 den1 <- den %>% filter(sector == "Parliament")
 
 # Er der flere kvinder i dette dataseæt? ----------------------------------
 
-# data.table
-gender1 <- den1[,.N,.(gender)]
-gender1$Percent <- round(gender1$N/sum(gender1$N), 2)*100
-gender1
+den1 %>% 
+  count(gender, sort=TRUE) %>% 
+  mutate(Percent = round(n/sum(n), 2)*100)
 
-#tidyverse 
 den1 %>% 
   group_by(gender) %>% 
-  summarize(N = n()) %>% 
-  mutate(Percent = round(N/sum(N), 2)*100)
+  summarize(n = n()) %>% 
+  mutate(Percent = round(n/sum(n), 2)*100)
+
+# Svaret er nej, der er kun 16 percent kvinder i "Parliament" subset
 
 ### SPØRGSMÅL 4 ###
 # Lav et one-mode netværk af individuer og visualisere dette. -------------
 
 #lav graph
-incidence <- xtabs(~name + affiliation, sparse = T, data = den1)
+incidence <- xtabs(formula = ~name + affiliation, sparse = TRUE, data = den1)
 
 graph <- graph_from_incidence_matrix(incidence, directed = FALSE)
 
@@ -92,13 +88,13 @@ ind_graph <- graph_projection$proj1
 ind_graph %>% 
   ggraph(layout = "fr") +
   geom_edge_link0(color = "gray70") +
-  geom_node_point(size = 1.5) +
+  geom_node_point(size = 1) +
   theme_graph()
 
 ### SPØRGSMÅL 5 ###
 # Beskriv netværket -------------------------------------------------------
 
-# Hvem er outsider/insider 
+
 
 
 
